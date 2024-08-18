c94f6d617f4223bb6fedf99d5486b20ee36cc03c
Atelski <atelubmawuli@gmail.com>
2024-08-18 23:32:49 +0000

    Adding components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,9 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +17,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <WelcomeMessage />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
