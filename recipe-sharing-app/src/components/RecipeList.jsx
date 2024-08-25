7740905abc18b18d4bff947db4082b59b469ee24
Atelski <atelubmawuli@gmail.com>
2024-08-25 23:57:43 +0000

    Adding RecipeList

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,23 +1,31 @@
-// src/components/RecipeList.jsx
-import { Link } from 'react-router-dom';
-import { useRecipeStore } from './recipeStore';
+import useRecipeStore from "../components/recipeStore.js";
+import { Link } from "react-router-dom";
 
 const RecipeList = () => {
-  // Get the recipes from the store
-  const recipes = useRecipeStore((state) => state.recipes);
+    const {recipes, filteredRecipes}= useRecipeStore(
+        state => ({recipes: state.recipes, filteredRecipes: state.filteredRecipes})
+    );
+    const filteredList = recipes.filter((recipe) => recipe.title.toLowerCase().includes(searchTerm.toLowerCase()))
 
-  return (
-    <div>
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-          {/* Add a Link to the recipe details page */}
-          <Link to={`/recipes/${recipe.id}`}>View Details</Link>
+    return(
+        <div>
+            {recipes.map(recipe => (
+                <div key ={recipe.id}>
+                    <h3>{recipe.title}</h3>
+                    <p>{recipe.description}</p>
+
+                    <ul>
+                        <li key = {recipe.id}>
+                            <Link to={`/recipes/ ${recipe.id}`} >{recipe.title}</Link>
+                        </li>
+                    </ul>
+
+                </div>
+                
+            ))}
+
+
         </div>
-      ))}
-    </div>
-  );
-};
-
-export default RecipeList;
+    )
+}
+export default RecipeList
